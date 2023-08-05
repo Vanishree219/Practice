e9b843e12034a40cc77ae0b6b6ef9d3de42e9488
Vanishree <vanishree.s219@gmail.com>
2023-08-05 16:52:43 +0530

    numbertoword

--- DemoProject/src/DemoPackage/DemoClass.java
+++ DemoProject/src/DemoPackage/DemoClass.java
@@ -1,69 +1,17 @@
 package DemoPackage;
 
-public class DemoClass extends A {
-	
-	public static void method1() {
-		System.out.println("Child class");
-	}
+public class Demo {
 
 	public static void main(String[] args) {
-		A a = new A();
-		a.method1();
-		DemoClass d1 = new DemoClass();
-		d1.method1();
 		
-		A a1 = new DemoClass();
+		DemoMethods d = new DemoMethods();
 		
-		a1.method1();
-		
-		
+		//d.duplicateNumbers();
+//		d.stringCount();
+		d.numberToWords(101);
 		
 		
 
 	}
 
 }
-
-class A{
-	  public static void  method1() {
-		System.out.println("parent Class");
-	}
-}
-
-
- abstract class A1{
-	 
-	 abstract void m1();
- }
- 
- class A2 extends A1{
-void m1() {
- 
- }}
- 
- 
- 
- interface I1{
-	 void m2();
-	 void m3();
-	 
- }
- 
- 
- class Demo1 implements I1{
-
-	@Override
-	public void m2() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void m3() {
-		// TODO Auto-generated method stub
-		
-	}
-	 
-	 
- 
- }
--- DemoProject/src/DemoPackage/DemoMethods.java
+++ DemoProject/src/DemoPackage/DemoMethods.java
@@ -1,6 +1,15 @@
 package DemoPackage;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.ListIterator;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Scanner;
+import java.util.Set;
 
 public class DemoMethods {
 
@@ -32,6 +41,157 @@ public class DemoMethods {
 		
 		System.out.println(count);
 		
+		
+		
+	}
+	
+	public void stringCount() {
+		String s = "Vanishree";
+		
+		LinkedHashMap<Character, Integer> ma = new LinkedHashMap<>();
+		
+		for(int i=0;i<s.length();i++) {
+			if(ma.containsKey(s.charAt(i))==false) {
+				ma.put(s.charAt(i), 1);
+			}
+			else {
+				ma.put(s.charAt(i), ma.get(s.charAt(i))+1);
+			}
+		}
+		String s1="";
+		for(Map.Entry<Character, Integer> e :ma.entrySet()) {
+			String s2=String.valueOf(e.getKey())+String.valueOf(e.getValue());
+			s1=s1+s2;
+			
+		}
+		
+		Set se = (Set) ma.keySet();
+		ArrayList al2 = new ArrayList(se);
+		
+		Collection al = (Collection) ma.values();
+		ArrayList al3 = new ArrayList(al);
+		
+		StringBuilder s34 = new StringBuilder();
+		
+		for(int i=0;i<al.size();i++) {
+			String s3=String.valueOf(al2.get(i))+String.valueOf(al3.get(i));
+			
+			s34.append(s3);			
+		}
+		System.out.println(s34);
+	}
+	
+	public void numberToWords(int num) {
+		
+		String arr[] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "Nine", "Ten", "eleven", "twelv", "thirteen","fourteen", "fivteen",
+				"sixteen", "seventeen", "eighteen", "nineteem"};
+		
+		
+		
+		
+		LinkedHashMap<Integer, String> hm1 = new LinkedHashMap<>();
+		
+		for(int i=1;i<=arr.length;i++) {
+			hm1.put(i, arr[i-1]);
+			
+		
+		}
+		
+		LinkedHashMap<Integer, String> hm2 = new LinkedHashMap<>();
+		
+		String arr2[]= {"ten","twenty","thirty", "fourty", "fifty","sixty","seventy","eighty","ninety" };
+		
+		for(int i=1;i<=arr2.length;i++) {
+			hm2.put(i, arr2[i-1]);
+			
+		
+		}
+		LinkedHashMap<Integer, String> hm3 = new LinkedHashMap<>();
+		String arr3[]= {"onehundred", "twohundre","threehundred", "fourhundred", "fivehundred",
+				"sixhundred","sevenhundred", "eighthundred","ninehundred"};
+		
+		for(int i=1;i<=arr3.length;i++) {
+			hm3.put(i, arr3[i-1]);
+			
+		
+		}
+		
+		if(num<19) {
+			for(Map.Entry<Integer,String> me:hm1.entrySet()) {
+				
+				if(me.getKey()==num) {
+					System.out.println(me.getValue());
+				}
+				
+			}
+			
+		}
+		else if(num>19 && num<100) {
+			
+			String s1 = String.valueOf(num);
+			
+			
+			String twodigit = "";
+			for(Map.Entry<Integer,String> me:hm2.entrySet()) {
+				
+				if(String.valueOf(me.getKey()).equals(String.valueOf(s1.charAt(0)))) {
+					
+					
+					twodigit = me.getValue();
+				}
+				
+			}
+			
+			for(Map.Entry<Integer,String> me:hm1.entrySet()) {
+				
+				if(String.valueOf(me.getKey()).equals(String.valueOf(s1.charAt(1)))) {
+					
+					twodigit = twodigit+ me.getValue();
+				}
+				
+			}
+			
+			System.out.println(twodigit);
+			
+			
+			
+		}
+		else {
+			
+			String s1 = String.valueOf(num);
+			
+			
+			String threedidgit = "";
+			
+			for(Map.Entry<Integer,String> me:hm3.entrySet()) {
+				
+				if(String.valueOf(me.getKey()).equals(String.valueOf(s1.charAt(0)))) {
+					
+					threedidgit = me.getValue();
+				}
+				
+			}
+			for(Map.Entry<Integer,String> me:hm2.entrySet()) {
+				
+				if(String.valueOf(me.getKey()).equals(String.valueOf(s1.charAt(1)))) {
+					
+					threedidgit =threedidgit+ me.getValue();
+				}
+				
+			}
+			
+			for(Map.Entry<Integer,String> me:hm1.entrySet()) {
+				
+				if(String.valueOf(me.getKey()).equals(String.valueOf(s1.charAt(2)))) {
+					
+					threedidgit = threedidgit+ me.getValue();
+				}
+				
+			}
+			
+			System.out.println(threedidgit);
+			
+		}
 		
 		
 	}
